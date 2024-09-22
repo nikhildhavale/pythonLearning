a8e6c41f35b17cecb5ba31d0a0222047ee22772c
Nikhil Dhavale <nikhil_dhavale@hotmail.com>
2024-09-22 18:54:41 +0530

    first commit

---
+++ session_15_sept_2024.py
@@ -0,0 +1,83 @@
+# print("hello1")
+# print("hello2")
+# print("hello3")
+# print("hello4")
+# print("hello5")
+
+# if 5>2:
+#     print("hello")
+#     if 5>2:
+#         print("hello")
+
+
+# if 5>2:
+#     print("hello")
+#     print("hello")
+
+# x = 10.0
+# print(type(x))
+# print(x)
+# y = "satyaranjan"
+# print(type(y))
+# print(y)
+
+# this is a comment
+# print("hello")  # this is a comment
+# # this is a comment
+
+# # this is a comment
+# a = "satyaranjan" # this is a comment
+# # this is a comment
+
+# """
+# sldkjv
+# sdkf
+# sdf
+# sdfk
+# sd
+# sdfj
+# """
+
+# def my_custom_function(x):
+#     """shdgf"""
+#     c = x+5
+#     return c
+
+# x = "satyaranjan"
+# y = 'satyaranjan'
+# print(x)
+# print(y)
+
+# x = "satyaranjan"
+# x = 6
+# print(x)
+
+# x = "satyaranjan"
+# X = "Amit"
+# print(X)
+
+# Satyaranjan = 10
+# satyaranjan = 200
+# print(satyaranjan)
+
+
+# myvar = "satyaranjan"
+# my_var = "satyaranjan"
+# _my_var = "satyaranjan"
+# myVar = "satyaranjan"
+# MYVAR = "satyaranjan"
+# myvar2 = "satyaranjan"
+
+# 2myvar = "satyaranjan"
+# my-var = "satyaranjan"
+# my var = "satyaranajn"
+
+# Camelcase 
+# myVaribaleName = "john"
+
+# # PascalCase
+# MyVaribaleName = "John"
+
+# #snakecase
+# my_varibale_name = "john"
+# myvaribalename = "john"
---
+++ session_21_sept_2024.py
@@ -0,0 +1,406 @@
+# print("hello1")
+# print("hello2")
+# print("hello3")
+# print("hello4")
+# print("hello5")
+
+# if 5>2:
+#     print("hello")
+#     if 5>2:
+#         print("hello")
+
+
+# if 5>2:
+#     print("hello")
+#     print("hello")
+
+# x = 10.0
+# print(type(x))
+# print(x)
+# y = "satyaranjan"
+# print(type(y))
+# print(y)
+
+# this is a comment
+# print("hello")  # this is a comment
+# # this is a comment
+
+# # this is a comment
+# a = "satyaranjan" # this is a comment
+# # this is a comment
+
+# """
+# sldkjv
+# sdkf
+# sdf
+# sdfk
+# sd
+# sdfj
+# """
+
+# def my_custom_function(x):
+#     """shdgf"""
+#     c = x+5
+#     return c
+
+# x = "satyaranjan"
+# y = 'satyaranjan'
+# print(x)
+# print(y)
+
+# x = "satyaranjan"
+# x = 6
+# print(x)
+
+# x = "satyaranjan"
+# X = "Amit"
+# print(X)
+
+# Satyaranjan = 10
+# satyaranjan = 200
+# print(satyaranjan)
+
+
+# myvar = "satyaranjan"
+# my_var = "satyaranjan"
+# _my_var = "satyaranjan"
+# myVar = "satyaranjan"
+# MYVAR = "satyaranjan"
+# myvar2 = "satyaranjan"
+
+# 2myvar = "satyaranjan"
+# my-var = "satyaranjan"
+# my var = "satyaranajn"
+
+# Camelcase 
+# myVaribaleName = "john"
+
+# # PascalCase
+# MyVaribaleName = "John"
+
+# #snakecase
+# my_varibale_name = "john"
+# myvaribalename = "john"
+
+# Many values to multiple variable
+# x,y,z = "satyaranjan","amit","nidhi"
+# print(x)
+# print(y)
+# print(z)
+
+
+#one value to  multiple variable
+# x=y=z = "satyaranjan"
+# print(x)
+# print(y)
+# print(z)
+
+#unplack a collection
+# name = ["Amit","nidhi","satyaranjan"]
+# x,y,z = name
+# print(x)
+# print(y)
+# print(z)
+
+#output Variable
+# x = "python is awesome"
+# print(x)
+
+# x = "python"
+# y = "is"
+# z = "Awesome"
+
+# print(x,y,z)
+# print(x+y+z)
+
+# a = 10
+# b = 14
+# print(a+b)
+# print(a,b)
+# print(a,z)
+# print(a+z)
+
+#global Variable
+
+# x = "awesome"
+# def myfunction():
+#     print("python is "+x)
+# myfunction()
+# print(x) 
+
+
+
+# x = "awesome"
+# def myfunction():
+#     x = "Fantastic"
+#     print("python is "+x)
+# myfunction()
+# print(x) 
+
+
+# x = "awesome"
+# def myfunction():
+#     global x
+#     x = "Fantastic"
+#     print("python is "+x)
+# myfunction()
+# print(x) 
+
+# Python Data Types
+
+# Text Type:	            str
+# Numeric Types:	        int, float, complex
+# Sequence Types:	        list, tuple, range
+# Mapping Type:	            dict
+# Set Types:	            set, frozenset
+# Boolean Type:	            bool
+# Binary Types:	            bytes, bytearray, memoryview
+# None Type:	            NoneType
+
+# x = 5.0j
+# print(type(x))
+
+# Example	                                                                        Data Type	
+# x = "Hello World"	                                                                str	
+# x = 20	                                                                        int	
+# x = 20.5	                                                                        float	
+# x = 1j	                                                                        complex	
+# x = ["apple", "banana", "cherry"]	                                                list	
+# x = ("apple", "banana", "cherry")	                                                tuple	
+# x = range(6)	                                                                    range	
+# x = {"name" : "John", "age" : 36}	                                                dict	
+# x = {"apple", "banana", "cherry"}	                                                set	
+# x = frozenset({"apple", "banana", "cherry"})	                                    frozenset	
+# x = True	                                                                        bool	
+# x = b"Hello"	                                                                    bytes	
+# x = bytearray(5)	                                                                bytearray	
+# x = memoryview(bytes(5))	                                                        memoryview	
+# x = None	                                                                        NoneType
+
+#python Number
+
+# x = 1
+# y = 2.8
+# z = 1j
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+
+
+# int
+# x = 1
+# y = 12242334255463465
+# z = -67545788653
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+
+
+#float
+# x = 1.0
+# y = 122423.34255463465
+# z = -67545788653.0
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+
+# x = 37e56
+# y = 122423E34255463465
+# z = -67545788653.00e8
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+
+# complex
+
+# x = 3+5j
+# y = 7j
+# z = -6j
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+
+
+#type conversion
+
+# x = 1
+# y = 2.8
+# z = 1j
+
+# a = float(x)
+# b = int(y)
+# c = complex(x)
+# print(a)
+# print(type(a))
+# print(b)
+# print(type(b))
+# print(c)
+# print(type(c))
+
+# random Number
+# import random
+# print(random.randrange(1,100))
+
+# python casting
+# x = int(1)
+# print(x)
+# y = int(2.8)
+# print(y)
+# z = int("3")
+# print(z)
+
+
+# x = float(1)
+# print(x)
+# y = float(2.8)
+# print(y)
+# z = float("3")
+# print(z)
+# a = float("3.6")
+# print(a)
+
+# x = str(1)
+# print(x)
+# y = str(2.8)
+# print(y)
+# z = str("3")
+# print(z)
+# a = str("s1")
+# print(a)
+
+#python string
+
+# print("hello")
+# print('hello')
+
+# print("it's is alright")
+# print("he is called 'johny'")
+# print('he is called "johny"')
+
+# a = "hello"
+# print(a)
+
+# a = """ 
+# sdbsdb
+# skdfgh
+# skdhf
+# sdf
+# sdf
+# """
+# print(a)
+
+# a = '''
+# sdbsdb
+# skdfgh
+# skdhf
+# sdf
+# sdf
+# '''
+# print(a)
+
+#string are arrays
+
+# a = "hello world"
+# print(a[1])
+
+# a = "hello world"
+# for x in a:
+#     print(x)
+
+# a = "hello world"
+# print(len(a))
+
+# checking string
+# txt = "the best things is free"
+# print("free" in txt)
+
+# txt = "the best things is free"
+# print("frees" not in txt)
+
+# txt = "the best things is free"
+# if "frees" in txt:
+#     print("yes 'free' is exits")
+
+# txt = "the best things is free"
+# if "frees" not in txt:
+#     print("No 'frees' is exits")
+
+#python slicing string
+
+# b = "hello world"
+# print(b[2:7])
+
+# b = "hello world"
+# print(b[:7])
+
+# b = "hello world"
+# print(b[2:])
+
+# b = "hello world"
+# print(b[-6:-2])
+
+# Python Modify strings
+
+# a = "hello world"
+# print(a.upper())
+
+# a = "HELLO WORLD"
+# print(a.lower())
+
+# a = "    hello world   "
+# print(a.strip())
+
+
+# a = "hello world"
+# print(a.replace("l","0"))
+
+# a = "hello world"
+# print(a.split(" "))
+
+#Python concatenation
+
+# a = "hello"
+# b = "world"
+# c = a+b
+# print(c)
+
+
+# a = "hello"
+# b = "world"
+# c = a+" "+b
+# print(c)
+
+#python format string
+
+# age = 36
+# txt = "my name is john ,i am "+ age
+# print(txt)
+
+# age = 36
+# txt = f"my name is john ,i am {age}"
+# print(txt)
+
+
+# age = 36
+# txt = f"my name is john ,i am {age:.2f}"
+# print(txt)
+
+# def sun(a,b):
+#     return a+b
+# txt = f"my nnumber is {sun(10,20)}"
+# print(txt)
