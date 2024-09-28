3bf27abcae87b93a286a853be078c49d48dd8cce
Nikhil Dhavale <nikhil_dhavale@hotmail.com>
2024-09-28 15:26:30 +0530

    22 sept

---
+++ session_22_sept.py
@@ -0,0 +1,724 @@
+# print("hello1")
+# print("hello2")
+# print("hello3")
+# print("hello4")
+# print("hello5")
+
+# if 5>2:
+#     print("hello")
+#     if 5>2:
+#         print("hello")
+
+
+# if 5>2:
+#     print("hello")
+#     print("hello")
+
+# x = 10.0
+# print(type(x))
+# print(x)
+# y = "satyaranjan"
+# print(type(y))
+# print(y)
+
+# this is a comment
+# print("hello")  # this is a comment
+# # this is a comment
+
+# # this is a comment
+# a = "satyaranjan" # this is a comment
+# # this is a comment
+
+# """
+# sldkjv
+# sdkf
+# sdf
+# sdfk
+# sd
+# sdfj
+# """
+
+# def my_custom_function(x):
+#     """shdgf"""
+#     c = x+5
+#     return c
+
+# x = "satyaranjan"
+# y = 'satyaranjan'
+# print(x)
+# print(y)
+
+# x = "satyaranjan"
+# x = 6
+# print(x)
+
+# x = "satyaranjan"
+# X = "Amit"
+# print(X)
+
+# Satyaranjan = 10
+# satyaranjan = 200
+# print(satyaranjan)
+
+
+# myvar = "satyaranjan"
+# my_var = "satyaranjan"
+# _my_var = "satyaranjan"
+# myVar = "satyaranjan"
+# MYVAR = "satyaranjan"
+# myvar2 = "satyaranjan"
+
+# 2myvar = "satyaranjan"
+# my-var = "satyaranjan"
+# my var = "satyaranajn"
+
+# Camelcase 
+# myVaribaleName = "john"
+
+# # PascalCase
+# MyVaribaleName = "John"
+
+# #snakecase
+# my_varibale_name = "john"
+# myvaribalename = "john"
+
+# Many values to multiple variable
+# x,y,z = "satyaranjan","amit","nidhi"
+# print(x)
+# print(y)
+# print(z)
+
+
+#one value to  multiple variable
+# x=y=z = "satyaranjan"
+# print(x)
+# print(y)
+# print(z)
+
+#unplack a collection
+# name = ["Amit","nidhi","satyaranjan"]
+# x,y,z = name
+# print(x)
+# print(y)
+# print(z)
+
+#output Variable
+# x = "python is awesome"
+# print(x)
+
+# x = "python"
+# y = "is"
+# z = "Awesome"
+
+# print(x,y,z)
+# print(x+y+z)
+
+# a = 10
+# b = 14
+# print(a+b)
+# print(a,b)
+# print(a,z)
+# print(a+z)
+
+#global Variable
+
+# x = "awesome"
+# def myfunction():
+#     print("python is "+x)
+# myfunction()
+# print(x) 
+
+
+
+# x = "awesome"
+# def myfunction():
+#     x = "Fantastic"
+#     print("python is "+x)
+# myfunction()
+# print(x) 
+
+
+# x = "awesome"
+# def myfunction():
+#     global x
+#     x = "Fantastic"
+#     print("python is "+x)
+# myfunction()
+# print(x) 
+
+# Python Data Types
+
+# Text Type:	            str
+# Numeric Types:	        int, float, complex
+# Sequence Types:	        list, tuple, range
+# Mapping Type:	            dict
+# Set Types:	            set, frozenset
+# Boolean Type:	            bool
+# Binary Types:	            bytes, bytearray, memoryview
+# None Type:	            NoneType
+
+# x = 5.0j
+# print(type(x))
+
+# Example	                                                                        Data Type	
+# x = "Hello World"	                                                                str	
+# x = 20	                                                                        int	
+# x = 20.5	                                                                        float	
+# x = 1j	                                                                        complex	
+# x = ["apple", "banana", "cherry"]	                                                list	
+# x = ("apple", "banana", "cherry")	                                                tuple	
+# x = range(6)	                                                                    range	
+# x = {"name" : "John", "age" : 36}	                                                dict	
+# x = {"apple", "banana", "cherry"}	                                                set	
+# x = frozenset({"apple", "banana", "cherry"})	                                    frozenset	
+# x = True	                                                                        bool	
+# x = b"Hello"	                                                                    bytes	
+# x = bytearray(5)	                                                                bytearray	
+# x = memoryview(bytes(5))	                                                        memoryview	
+# x = None	                                                                        NoneType
+
+#python Number
+
+# x = 1
+# y = 2.8
+# z = 1j
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+
+
+# int
+# x = 1
+# y = 12242334255463465
+# z = -67545788653
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+
+
+#float
+# x = 1.0
+# y = 122423.34255463465
+# z = -67545788653.0
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+
+# x = 37e56
+# y = 122423E34255463465
+# z = -67545788653.00e8
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+
+# complex
+
+# x = 3+5j
+# y = 7j
+# z = -6j
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+
+
+#type conversion
+
+# x = 1
+# y = 2.8
+# z = 1j
+
+# a = float(x)
+# b = int(y)
+# c = complex(x)
+# print(a)
+# print(type(a))
+# print(b)
+# print(type(b))
+# print(c)
+# print(type(c))
+
+# random Number
+# import random
+# print(random.randrange(1,100))
+
+# python casting
+# x = int(1)
+# print(x)
+# y = int(2.8)
+# print(y)
+# z = int("3")
+# print(z)
+
+
+# x = float(1)
+# print(x)
+# y = float(2.8)
+# print(y)
+# z = float("3")
+# print(z)
+# a = float("3.6")
+# print(a)
+
+# x = str(1)
+# print(x)
+# y = str(2.8)
+# print(y)
+# z = str("3")
+# print(z)
+# a = str("s1")
+# print(a)
+
+#python string
+
+# print("hello")
+# print('hello')
+
+# print("it's is alright")
+# print("he is called 'johny'")
+# print('he is called "johny"')
+
+# a = "hello"
+# print(a)
+
+# a = """ 
+# sdbsdb
+# skdfgh
+# skdhf
+# sdf
+# sdf
+# """
+# print(a)
+
+# a = '''
+# sdbsdb
+# skdfgh
+# skdhf
+# sdf
+# sdf
+# '''
+# print(a)
+
+#string are arrays
+
+# a = "hello world"
+# print(a[1])
+
+# a = "hello world"
+# for x in a:
+#     print(x)
+
+# a = "hello world"
+# print(len(a))
+
+# checking string
+# txt = "the best things is free"
+# print("free" in txt)
+
+# txt = "the best things is free"
+# print("frees" not in txt)
+
+# txt = "the best things is free"
+# if "frees" in txt:
+#     print("yes 'free' is exits")
+
+# txt = "the best things is free"
+# if "frees" not in txt:
+#     print("No 'frees' is exits")
+
+#python slicing string
+
+# b = "hello world"
+# print(b[2:7])
+
+# b = "hello world"
+# print(b[:7])
+
+# b = "hello world"
+# print(b[2:])
+
+# b = "hello world"
+# print(b[-6:-2])
+
+# Python Modify strings
+
+# a = "hello world"
+# print(a.upper())
+
+# a = "HELLO WORLD"
+# print(a.lower())
+
+# a = "    hello world   "
+# print(a.strip())
+
+
+# a = "hello world"
+# print(a.replace("l","0"))
+
+# a = "hello world"
+# print(a.split(" "))
+
+#Python concatenation
+
+# a = "hello"
+# b = "world"
+# c = a+b
+# print(c)
+
+
+# a = "hello"
+# b = "world"
+# c = a+" "+b
+# print(c)
+
+#python format string
+
+# age = 36
+# txt = "my name is john ,i am "+ age
+# print(txt)
+
+# age = 36
+# txt = f"my name is john ,i am {age}"
+# print(txt)
+
+
+# age = 36
+# txt = f"my name is john ,i am {age:.2f}"
+# print(txt)
+
+# def sun(a,b):
+#     return a+b
+# txt = f"my nnumber is {sun(10,20)}"
+# print(txt)
+
+# Python - Escape Characters
+
+# txt  = "we are the so=called \"vikings\" from the north"
+# print(txt)
+
+
+# Code	Result	
+# \'	Single Quote	
+# \\	Backslash	
+# \n	New Line	
+# \r	Carriage Return	
+# \t	Tab	
+# \b	Backspace	
+# \f	Form Feed	
+# \ooo	Octal value	
+# \xhh	Hex value	
+
+# Python - String Methods
+# Method	                        Description
+# capitalize()	                    Converts the first character to upper case
+# casefold()	                    Converts string into lower case
+# center()	                        Returns a centered string
+# count()	                        Returns the number of times a specified value occurs in a string
+# encode()	                        Returns an encoded version of the string
+# endswith()	                    Returns true if the string ends with the specified value
+# expandtabs()	                    Sets the tab size of the string
+# find()	                        Searches the string for a specified value and returns the position of where it was found
+# format()	                        Formats specified values in a string
+# format_map()	                    Formats specified values in a string
+# index()	                        Searches the string for a specified value and returns the position of where it was found
+# isalnum()	                        Returns True if all characters in the string are alphanumeric
+# isalpha()	                        Returns True if all characters in the string are in the alphabet
+# isascii()	                        Returns True if all characters in the string are ascii characters
+# isdecimal()	                    Returns True if all characters in the string are decimals
+# isdigit()	                        Returns True if all characters in the string are digits
+# isidentifier()	                Returns True if the string is an identifier
+# islower()	                        Returns True if all characters in the string are lower case
+# isnumeric()	                    Returns True if all characters in the string are numeric
+# isprintable()	                    Returns True if all characters in the string are printable
+# isspace()	                        Returns True if all characters in the string are whitespaces
+# istitle()	                        Returns True if the string follows the rules of a title
+# isupper()	                        Returns True if all characters in the string are upper case
+# join()	                        Joins the elements of an iterable to the end of the string
+# ljust()	                        Returns a left justified version of the string
+# lower()	                        Converts a string into lower case
+# lstrip()	                        Returns a left trim version of the string
+# maketrans()	                    Returns a translation table to be used in translations
+# partition()	                    Returns a tuple where the string is parted into three parts
+# replace()	                        Returns a string where a specified value is replaced with a specified value
+# rfind()	                        Searches the string for a specified value and returns the last position of where it was found
+# rindex()	                        Searches the string for a specified value and returns the last position of where it was found
+# rjust()	                        Returns a right justified version of the string
+# rpartition()	                    Returns a tuple where the string is parted into three parts
+# rsplit()	                        Splits the string at the specified separator, and returns a list
+# rstrip()	                        Returns a right trim version of the string
+# split()	                        Splits the string at the specified separator, and returns a list
+# splitlines()	                    Splits the string at line breaks and returns a list
+# startswith()	                    Returns true if the string starts with the specified value
+# strip()	                        Returns a trimmed version of the string
+# swapcase()	                    Swaps cases, lower case becomes upper case and vice versa
+# title()	                        Converts the first character of each word to upper case
+# translate()	                    Returns a translated string
+# upper()	                        Converts a string into upper case
+# zfill()	                        Fills the string with a specified number of 0 values at the beginning
+
+# Python booleans
+
+# print(10<9)
+# print(10==10)
+# print(10>9)
+
+# a = 200
+# b = 33
+# if b>a:
+#     print("b is greater than a")
+# else:
+#     print("a is greater than b")
+
+# print(bool("hello"))
+# print(bool(17))
+# x = "hello"
+# y = 10
+# print(bool(x))
+# print(bool(y))
+
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool(""))
+# print(bool(()))
+# print(bool([]))
+# print(bool({}))
+
+# class myclass():
+#     def __len__(self):
+#         return 1
+    
+# myobj = myclass()
+# print(bool(myobj))
+
+# def myfunnction():
+#     return False
+
+# if myfunnction():
+#     print("yes")
+# else:
+#     print("no")
+
+# x = "200"
+# print(isinstance(x,int))
+
+#python operators
+
+# Arithmetic operators
+
+# Operator	        Name	                            Example	
+# +	                Addition	                        x + y	
+# -	                Subtraction	                        x - y	
+# *	                Multiplication	                    x * y	
+# /	                Division	                        x / y	
+# %	                Modulus	                            x % y	
+# **	            Exponentiation	                    x ** y	
+# //	            Floor division	                    x // y
+
+# Assignment operators
+
+# Operator	        Example	                        Same As
+# =	                x = 5	                        x = 5	
+# +=	            x += 3	                        x = x + 3	
+# -=	            x -= 3	                        x = x - 3	
+# *=	            x *= 3	                        x = x * 3	
+# /=	            x /= 3	                        x = x / 3	
+# %=	            x %= 3	                        x = x % 3	
+# //=	            x //= 3	                        x = x // 3	
+# **=	            x **= 3	                        x = x ** 3	
+# &=	            x &= 3	                        x = x & 3	
+# |=	            x |= 3	                        x = x | 3	
+# ^=	            x ^= 3	                        x = x ^ 3	
+# >>=	            x >>= 3	                        x = x >> 3	
+# <<=	            x <<= 3	                        x = x << 3	
+# :=	            print(x := 3)	                x = 3  print(x)
+#                                               
+
+# Comparison operators
+
+# Operator	            Name	                                Example	
+# ==	                Equal	                                x == y	
+# !=	                Not equal	                            x != y	
+# >	                    Greater than	                        x > y	
+# <	                    Less than	                            x < y	
+# >=	                Greater than or equal to	            x >= y	
+# <=	                Less than or equal to	                x <= y
+
+# Logical operators
+
+# Operator	    Description	                                                        Example	
+# and 	        Returns True if both statements are true	                        x < 5 and  x < 10	
+# or	        Returns True if one of the statements is true	                    x < 5 or x < 4	
+# not	        Reverse the result, returns False if the result is true	            not(x < 5 and x < 10)
+
+
+# Identity operators
+
+# Operator	    Description	                                                    Example	
+# is 	        Returns True if both variables are the same object	            x is y	
+# is not	    Returns True if both variables are not the same object	        x is not y
+
+# Membership operators
+
+# Operator	    Description	                                                                                Example
+# in 	        Returns True if a sequence with the specified value is present in the object	            x in y	
+# not in	    Returns True if a sequence with the specified value is not present in the object	        x not in y	
+
+
+# Bitwise operators
+
+# Operator	    Name	                        Description	                                                                                                            Example	
+# & 	        AND	                            Sets each bit to 1 if both bits are 1	                                                                                x & y	
+# |	            OR	                            Sets each bit to 1 if one of two bits is 1	                                                                            x | y	
+# ^	            XOR	                            Sets each bit to 1 if only one of two bits is 1	                                                                        x ^ y	
+# ~	            NOT	                            Inverts all the bits	                                                                                                ~x	
+# <<	        Zero fill left shift	        Shift left by pushing zeros in from the right and let the leftmost bits fall off	                                    x << 2	
+# >>	        Signed right shift	            Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off	                x >> 2
+
+# print(10+(19-5)*(14+3)+1/10)
+
+# Python Lists
+# mylist = ["apple","banana","cherry"]
+# print(mylist)
+# print(len(mylist))
+
+# mylist1 = ["apple","banana","cherry"]
+# mylist2 = [1,2,3,8,6,10]
+# mylist3 = [True,False,True,False]
+
+# mylist = [1,"apple",3.0,False]
+
+# mynewlist = list(("apple","banana","cherry"))
+# print(mynewlist)
+
+# mylist = ["apple","banana","cherry"]
+# print(mylist[1])
+
+# mylist = ["apple","banana","cherry"]
+# print(mylist[-1])
+
+
+# mylist = ["apple","banana","cherry","apple","banana","cherry","apple","banana","cherry"]
+# print(mylist[2:6])
+
+
+# mylist = ["apple","banana","cherry","apple","banana","cherry","apple","banana","cherry"]
+# print(mylist[:6])
+
+# mylist = ["apple","banana","cherry","apple","banana","cherry","apple","banana","cherry"]
+# print(mylist[2:])
+
+# mylist = ["apple","banana","cherry","apple","banana","cherry","apple","banana","cherry"]
+# print(mylist[-4:-1])
+
+
+# mylist = ["apple","banana","cherry"]
+# if "apples" in mylist:
+#     print("yes apple is exits")
+
+# Python change list items
+# mylist = ["apple","banana","cherry"]
+# mylist[1] = "orange"
+# print(mylist)
+
+
+# mylist = ["apple","banana","cherry","banana","cherry","banana","cherry"]
+# mylist[1:3] = ["orange"]
+# print(mylist)
+
+# mylist = ["apple","banana","cherry","banana","cherry","banana","cherry"]
+# mylist[1:3] = ["orange","waterlemon","blackcurrent"]
+# print(mylist)
+
+# mylist = ["apple","banana","cherry"]
+# mylist.insert(0,"orange")
+# print(mylist)
+
+# mylist = ["apple","banana","cherry"]
+# mylist.append("orange")
+# print(mylist)
+
+#python extend list
+
+# mylist = ["apple","banana","cherry"]
+# mmylist1 = ["orange","waterlemon","blackcurrent"]
+# mylist.extend(mmylist1)
+# print(mylist)
+
+# mylist = ["apple","banana","cherry"]
+# mmylist1 = ("orange","waterlemon","blackcurrent")
+# mylist.extend(mmylist1)
+# print(mylist)
+
+#python remove list items
+# mylist = ["apple","banana","cherry"]
+# mylist.remove("apple")
+# print(mylist)
+
+
+# mylist = ["apple","banana","cherry","apple"]
+# mylist.remove("apple")
+# print(mylist)
+
+# mylist = ["apple","banana","cherry","apple"]
+# mylist.pop(1)
+# print(mylist)
+
+# mylist = ["apple","banana","cherry","apple"]
+# mylist.pop()
+# print(mylist)
+
+
+# mylist = ["apple","banana","cherry","apple"]
+# del mylist[1]
+# print(mylist)
+
+# mylist = ["apple","banana","cherry","apple"]
+# del mylist
+# print(mylist)
+
+# mylist = ["apple","banana","cherry","apple"]
+# mylist.clear()
+# print(mylist) 
+
+#python loop list
+#  for loop
+# mylist = ["apple","banana","cherry","apple"]
+# for x in mylist:
+#     print(x)
+    
+    
+# mylist = ["apple","banana","cherry","apple"]
+# for x in range(len(mylist)):
+#     print(x)
+#     print(mylist[x])
+
+# while loops
+# i = 0
+# while True:
+#     print(i)
+#     i = i+1
+
+# mylist = ["apple","banana","cherry","apple"]
+# i = 0
+# while i < len(mylist):
+#     print(mylist[i])
+#     i = i+1
+
+# list comprehensionn
+# mylist = ["apple","banana","cherry","apple"]
+# [print(x) for x in mylist]
